2fe24bd5454a1a796efe940fd522cfea133db411
âsusuzimo <1432625686@qq.com>
2019-11-19 18:23:44 +0800

    创建项目

---
+++ src/main/java/com/jianghu/vhr/VhrApplication.java
@@ -0,0 +1,13 @@
+package com.jianghu.vhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VhrApplication.class, args);
+	}
+
+}
